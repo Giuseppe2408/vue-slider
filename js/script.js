4be2426061d3e20f874529dbdbe1aa5ee97f3c35
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-25 17:21:43 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-// const slides = [
-//     {
-//         image: 'img/01.jpg',
-//         title: 'Svezia',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     },
-//     {
-//         image: 'img/02.jpg',
-//         title: 'Svizzera',
-//         text: 'Lorem ipsum.',
-//     },
-//     {
-//         image: 'img/03.jpg',
-//         title: 'Gran Bretagna',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     },
-//     {
-//         image: 'img/04.jpg',
-//         title: 'Germania',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-//     },
-//     {
-//         image: 'img/05.jpg',
-//         title: 'Paradise',
-//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-//     }
-// ];
-
-// console.log(slides);
+slides = [
+    {
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        image: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum.',
+    },
+    {
+        image: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+    },
+    {
+        image: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+    }
+];
 
 const myApp = new Vue({
     el: "#app",
@@ -60,20 +58,27 @@ const myApp = new Vue({
             }
         ],
     },
+    // bottoni avanti indietro
     methods : {
         mynext() {
             this.activeslide++;
             console.log(this.activeslide);
-            if (this.activeslide > this.slides.lenght - 1) {
+            if (this.activeslide > 5 - 1) {
                 this.activeslide = 0;
             }
+            console.log(activeslide);
         },
         myprev(){
             this.activeslide--;
-            if (this.activeslide < this.slides.lenght) {
-                
+            if (this.activeslide < 0) {
+                this.activeslide = 5 - 1;
             }
-        }
+            console.log(activeslide);
+        },
+        switchSlide(activeslide) {
+            this.activeslide = activeslide;
+         }
+     
     }
             
     
